02c3c1226ba68d5d1869cfc1cbdf4fe5ef422072
chanki00 <rlacksrl00@naver.com>
2024-06-14 22:45:44 +0900

    feat: 생성, 조회 API JdbcTemplate으로 변경

--- library-app/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,32 +4,45 @@ import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {  //의존성 주입
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge()); //위에 ?에 순서대로 각각 데이터가 순서대로(이름, 나이) 매핑된다.
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i=0; i<users.size(); ++i) {
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-
-        return responses;
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {  //jdbcTemplate의 쿼리가 sql을 수행한 결과를 UserResponse로 바꿔주는 역할
+                long id = rs.getLong("id"); //rs가 쿼리 결과임
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
     }
 
 
